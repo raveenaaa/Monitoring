b02b644d9d906ce4610cd5c210927939f0d566d5
Chris Parnin <chris.parnin@gmail.com>
2020-04-07 16:44:39 -0400

    remove dead code.

--- dashboard/metrics/index.js
+++ dashboard/metrics/index.js
@@ -12,19 +12,8 @@ var servers =
 
 function start(app)
 {
-	// const server = require('http').createServer(app);
-	// const sio = require("socket.io")(server, {
-	//   handlePreflightRequest: (req, res) => {
-	// 	  const headers = {
-	// 		  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-	// 		  "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-	// 		  "Access-Control-Allow-Credentials": true
-	// 	  };
-	// 	  res.writeHead(200, headers);
-	// 	  res.end();
-	//   }
-	// });
 	const io = require('socket.io')(3000);
+	// force websocket protocol, otherwise some browsers may try polling.
 	io.set('transports', ['websocket']);
 	io.on('connection', function (socket) {
         console.log(`Received connection id ${socket.id} connected ${socket.connected}`);
