01a8ae3b355a0246d9994f26859e5e79f85755a5
Chris Parnin <chris.parnin@gmail.com>
2020-04-07 17:38:06 -0400

    push agent code to servers.

--- servers/lib/VBoxManage.js
+++ servers/lib/VBoxManage.js
@@ -31,29 +31,50 @@ module.exports.execute = function(cmd, args) {
 
 };
 
-module.exports.show = async function(vmname) {
-        return new Promise(function (resolve, reject) {   
-            exec(`${VBexe} showvminfo ${vmname} --machinereadable`, (error, stdout, stderr) => {
-                if(error && stderr.indexOf('VBOX_E_OBJECT_NOT_FOUND') != -1) {
-                    resolve({VMState:'not_found'});
+module.exports.getSSHPort = async function(vmname)
+{
+    let properties = await getProperties(vmname);
+    // Use VirtualBox driver
+    Object.keys(properties).forEach(key => {
+        if(properties[key].includes('guestssh')){
+            port = parseInt( properties[key].split(',')[3]);
+        }
+    });
+    return port;
+}
+
+async function getProperties(vmname)
+{
+    return new Promise(function (resolve, reject) {   
+        exec(`${VBexe} showvminfo ${vmname} --machinereadable`, (error, stdout, stderr) => {
+            if(error && stderr.indexOf('VBOX_E_OBJECT_NOT_FOUND') != -1) {
+                resolve({VMState:'not_found'});
+            }
+            else if( error )
+            {
+                console.error(`=> ${error}, ${stderr}`);
+                reject(error);
+            }
+            else
+            {
+                let properties = {state:'unknown'};
+                let lines = stdout.split('\n');
+                for (let i = 0; i < lines.length-1; i++) {
+                    let lineSplit = lines[i].split('=');
+                    let name= lineSplit[0].trim();
+                    let id = lineSplit[1].trim();
+                    properties[name]=id.toString();
                 }
-                else if( error )
-                {
-                    console.error(`=> ${error}, ${stderr}`);
-                    reject(error);
-                }
-                else
-                {
-                    let properties = {state:'unknown'};
-                    let lines = stdout.split('\n');
-                    for (let i = 0; i < lines.length-1; i++) {
-                        let lineSplit = lines[i].split('=');
-                        let name= lineSplit[0].trim();
-                        let id = lineSplit[1].trim();
-                        properties[name]=id.toString();
-                    }
-                    resolve(properties.VMState.replace(/"/g,''));
-                }
-            });
+                resolve(properties);
+            }
         });
-    }
+    });
+}
+
+
+module.exports.show = async function(vmname) {
+
+    let properties = await getProperties(vmname);
+    return properties.VMState.replace(/"/g,'')
+   
+}
--- servers/lib/scp.js
+++ servers/lib/scp.js
@@ -3,11 +3,14 @@ const os   = require('os');
 
 const child = require('child_process');
 
-let identifyFile = path.join(os.homedir(), '.bakerx', 'insecure_private_key');
+let identifyFile = path.join(os.homedir(), '.bakerx', 'baker_rsa');
 
-module.exports = function(src, dest) {
+module.exports = function(port, src, dest) {
     
     let scpArgs = [];
+    scpArgs.push(`-q`);
+    scpArgs.push(`-P`);
+    scpArgs.push(`${port}`);
     scpArgs.push(`-i`);
     scpArgs.push(`"${identifyFile}"`)
     scpArgs.push(`-o`);
@@ -16,6 +19,6 @@ module.exports = function(src, dest) {
     scpArgs.push(`UserKnownHostsFile=/dev/null`);
     scpArgs.push(`"${src}"`);
     scpArgs.push(`"${dest}"`);        
-    console.log(scpArgs);
+    // console.log(scpArgs);
     return child.spawnSync(`scp`, scpArgs, {stdio: 'inherit', shell: true});
 }
