dd8346d4561c816831ccc6b528a8036c66e5dae6
Chris Parnin <chris.parnin@gmail.com>
2020-04-08 11:54:22 -0400

    final flow

--- dashboard/metrics/index.js
+++ dashboard/metrics/index.js
@@ -1,22 +1,30 @@
-// websocket server that website connects to.
-const os = require('os');
-const request = require('request');
+// websocket server that dashboard connects to.
 const redis = require('redis');
+const got = require('got');
+const fs = require('fs');
+const path = require('path');
+
+// We need your host computer ip address in order to use port forwards to servers.
+let ip = ''
+try
+{
+	ip = fs.readFileSync(path.join(__dirname,'ip.txt')).toString();
+}
+catch(e)
+{
+	console.log(e);
+	throw new Error("Missing required ip.txt file");	
+}
 
 /// Servers data being monitored.
 var servers = 
 [
-	{name: "computer", memoryLoad: 0},
-	{name: "alpine-01",url:"http://localhost:9001", latency: 0, cpu: 0, memoryLoad: 0, status: "#cccccc"},
-	{name: "alpine-02",url:"http://localhost:9002", latency: 0, cpu: 0, memoryLoad: 0, status: "#cccccc"},
-	{name: "alpine-03",url:"http://localhost:9003", latency: 0, cpu: 0, memoryLoad: 0, status: "#cccccc"}
+	{name: "computer", status: "#cccccc", scoreTrend : []},
+	{name: "alpine-01",url:`http://${ip}:9001/`, status: "#cccccc",  scoreTrend : [0]},
+	{name: "alpine-02",url:`http://${ip}:9002/`, status: "#cccccc",  scoreTrend : [0]},
+	{name: "alpine-03",url:`http://${ip}:9003/`, status: "#cccccc",  scoreTrend : [0]}
 ];
 
-///////////////////////////////////////////////////////////////////////////////////////
-// REDIS
-///////////////////////////////////////////////////////////////////////////////////////
-
-let client = redis.createClient(6379, 'localhost', {});
 
 function start(app)
 {
@@ -24,20 +32,21 @@ function start(app)
 	// DASHBOARD
 	////////////////////////////////////////////////////////////////////////////////////////
 	const io = require('socket.io')(3000);
-	// force websocket protocol, otherwise some browsers may try polling.
+	// Force websocket protocol, otherwise some browsers may try polling.
 	io.set('transports', ['websocket']);
+	// Whenever a new page/client opens a dashboard, we handle the request for the new socket.
 	io.on('connection', function (socket) {
         console.log(`Received connection id ${socket.id} connected ${socket.connected}`);
 
 		if( socket.connected )
 		{
-			//// Broadcast heartbeat over websockets ever 1 second
+			//// Broadcast heartbeat event over websockets ever 1 second
 			var heartbeatTimer = setInterval( function () 
 			{
-				// console.log("interval", servers)
 				socket.emit("heartbeat", servers);
 			}, 1000);
 
+			//// If a client disconnects, we will stop sending events for them.
 			socket.on('disconnect', function (reason) {
 				console.log(`closing connection ${reason}`);
 				clearInterval(heartbeatTimer);
@@ -48,28 +57,85 @@ function start(app)
 	/////////////////////////////////////////////////////////////////////////////////////////
 	// REDIS SUBSCRIPTION
 	/////////////////////////////////////////////////////////////////////////////////////////
+	let client = redis.createClient(6379, 'localhost', {});
+	// We subscribe to all the data being published by the server's metric agent.
+	for( var server of servers )
+	{
+		// The name of the server is the name of the channel to recent published events on redis.
+		client.subscribe(server.name);
+	}
+
+	// When an agent has published information to a channel, we will receive notification here.
 	client.on("message", function (channel, message) 
 	{
-		console.log(`Received message from ${channel}`)
+		console.log(`Received message from agent: ${channel}`)
 		for( var server of servers )
 		{
+			// Update our current snapshot for a server's metrics.
 			if( server.name == channel)
 			{
 				let payload = JSON.parse(message);
 				server.memoryLoad = payload.memoryLoad;
 				server.cpu = payload.cpu;
-				server.disk = payload.disk;
+				updateHealth(server);
 			}
 		}
 	});
 
-	for( var server of servers )
+	// LATENCY CHECK
+	var latency = setInterval( function () 
 	{
-		client.subscribe(server.name);
+		for( var server of servers )
+		{
+			if( server.url )
+			{
+				let now = Date.now();
+
+				// Bind a new variable in order to for it to be properly captured inside closure.
+				let captureServer = server;
+
+				// Make request to server we are monitoring.
+				got(server.url, {timeout: 5000, throwHttpErrors: false}).then(function(res)
+				{
+					captureServer.statusCode = res.statusCode;
+					captureServer.latency = Date.now() - now;
+				}).catch( e => 
+				{
+					// console.log(e);
+					captureServer.statusCode = e.code;
+					captureServer.latency = 5000;
+				});
+			}
+		}
+	}, 10000);
+}
+
+function updateHealth(server)
+{
+	let score = 0;
+	score += server.memoryLoad < 25.00 ? 1: 0;
+	score += server.cpu < 25.00 ? 1: 0;
+	score += server.latency < 100 ? 1: 0;
+	score += server.statusCode === 200 ? 1: 0
+
+	server.status = score2color(score/4);
+
+
+	console.log(`${server.name} ${score}`);
+	// Add score to trend data.
+	server.scoreTrend.push( (4-score));
+	if( server.scoreTrend.length > 100 )
+	{
+		server.scoreTrend.shift();
 	}
+}
 
-
-
+function score2color(score)
+{
+	if (score <= 0.25) return "#ff0000";
+	if (score <= 0.50) return "#ffcc00";
+	if (score <= 0.75) return "#00cc00";
+	return "#00ff00";
 }
 
 module.exports.start = start;
